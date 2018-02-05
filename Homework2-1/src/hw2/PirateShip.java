ea9848b079d7eb4ad2b131ba9b6cdf7d6e2e3e8c
ahinojo1 <ahinojo1@mail.depaul.edu>
2018-02-04 21:59:13 -0600

    .

---
+++ Homework2-1/src/hw2/PirateShip.java
@@ -0,0 +1,12 @@
+package hw2;
+
+public class PirateShip {
+	public void main(String[] args) {
+		System.out.println("hello i am a pirate");
+		word();
+	}
+	
+	public String word() {
+		return "henlo";
+	}
+}
