6d46d2ced00597c5612e8f575f3989e81cb6576f
ahinojo1 <ahinojo1@mail.depaul.edu>
2018-02-17 20:17:08 -0600

    ShapeComposer

--- Homework3/src/HW3/ShapeComposer.java
+++ Homework3/src/HW3/ShapeComposer.java
@@ -1,10 +1,18 @@
 package HW3;
 
-public class ShapeComposer {
+import javafx.application.*;
+import javafx.stage.Stage;
+public class ShapeComposer extends Application{
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+			launch(args);
+	}
 
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
